b63ff122e7817e32ea2ffff9e438fa6469d3723c
Can Utku <33282009+cutku@users.noreply.github.com>
2017-12-19 07:38:25 +0300

    Login-Sign
    Base part Completed
    login-logout

--- Project/e-lection/src/app/app.component.ts
+++ Project/e-lection/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    moduleId: module.id,
+    selector: 'app',
+    templateUrl: 'app.component.html'
 })
+
 export class AppComponent {
-  title = 'app';
-}
+    title = 'E-Lection'
+    title2 = 'BETA TEST'
+
+ }
