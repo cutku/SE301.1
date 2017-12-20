f6cdbb5c1ef6ec3b937ca1ccc8e3792305755926
Can Utku <33282009+cutku@users.noreply.github.com>
2017-12-20 08:47:14 +0300

    Last ------

---
+++ Project/E-lection-v1.2/src/app/app.component.ts
@@ -0,0 +1,16 @@
+﻿import { Component } from '@angular/core';
+
+import '../assets/app.css';
+
+declare var $:any;
+@Component({
+    moduleId: module.id.toString(),
+    selector: 'app',
+    templateUrl: 'app.component.html'
+})
+
+export class AppComponent {
+    title = 'E-Lection'
+    title2 = 'BETA TEST'
+
+ }
