17bf749d0e4f2d93d64f5fce0117571adf8db012
Maskoliver <33281901+Maskoliver@users.noreply.github.com>
2017-12-30 17:07:04 +0300

    asd

--- Project/E-lection-v1.2/src/app/testt/testt.component.ts
+++ Project/E-lection-v1.2/src/app/testt/testt.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+
 declare interface TesttData {
     headerRow: string[];
     dataRows: string[][];
+
+  
 }
 
 @Component({
@@ -14,28 +17,9 @@ declare interface TesttData {
 export class Testt implements OnInit{
     public tableData1: TesttData;
     public tableData2: TesttData;
+
+    
     ngOnInit(){
-        this.tableData1 = {
-            headerRow: [ 'Ranking', 'Name', 'Country', 'City', 'Votes'],
-            dataRows: [
-                ['1', 'Dakota Rice', 'Niger', 'Oud-Turnhout', '3738'],
-                ['2', 'Minerva Hooper', 'Curaçao', 'Sinaai-Waas', '2789'],
-                ['3', 'Sage Rodriguez', 'Netherlands', 'Baileux', '1542'],
-                ['4', 'Philip Chaney', 'Korea, South', 'Overland Park', '875'],
-                ['5', 'Doris Greene', 'Malawi', 'Feldkirchen in Kärnten', '642'],
-                ['6', 'Mason Porter', 'Chile', 'Gloucester', '501']
-            ]
-        };
-        this.tableData2 = {
-            headerRow: [ 'Ranking', 'Name', 'Country', 'City', 'Votes'],
-            dataRows: [
-                ['1', 'Dakota Hawkins', 'Niger', 'Oud-Turnhout', '480'],
-                ['2', 'Molly Hooper', 'England', 'London', '400'],
-                ['3', 'Hermonie Granger', 'Netherlands', 'Baileux', '370'],
-                ['4', 'Lee Ho', 'China', 'Bejing', '236'],
-                ['5', 'Dorry Greene', 'Malawi', 'Feldkirchen in Kärnten', '126'],
-                ['6', 'Harry Potter', 'Chile', 'Gloucester', '002']
-            ]
-        };
+        
     }
 }
