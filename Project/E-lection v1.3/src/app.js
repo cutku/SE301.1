0e9ab440f84f04147d5919f2766cc657daf2e88b
Maskoliver <33281901+Maskoliver@users.noreply.github.com>
2018-01-03 20:10:11 +0300

    asd

---
+++ Project/E-lection v1.3/src/app.js
@@ -0,0 +1,11 @@
+(function () {
+    var app = angular.module('store', []);
+    app.controller('StoreController', function () {
+    });
+
+    var gem = {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: '. . .',
+    }.
+})();
